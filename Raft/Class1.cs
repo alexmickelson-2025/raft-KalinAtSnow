acb30d7a2015814cc8d39320181bfe56f49330ad
kalin <kalin@KALINDESKTOP>
2025-01-11 16:55:02 -0700

    Add project files.

---
+++ Raft/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Raft
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ RaftTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace RaftTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
