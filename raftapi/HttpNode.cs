72260227d7b300d07c4987aa44c9c64abb347d7b
Kalin Nebeker <Kalin.Nebeker@yahoo.com>
2025-01-29 15:14:07 -0700

    added project prepping for http nodes

---
+++ raftapi/HttpNode.cs
@@ -0,0 +1,16 @@
+﻿using Raft;
+
+namespace raftapi;
+
+public class HttpNode: INode
+{
+    public int _id { get; }
+    public string Url { get; }
+    private HttpClient client = new();
+
+    public HttpNode(int id, string url)
+    {
+        _id = id;
+        Url = url;
+    }
+}
